c5fe577e2e722c8271227ab4c6f35a44edee9946
gpanda70 <gpanda@uga.edu>
2018-09-17 19:34:33 -0400

    added json and requests

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import flask
 import os
 from random import randint
+import requests
+import json
 
 server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
@@ -9,4 +11,13 @@ bot_id = 'c16cfe66242b8d31fb901bce3e'
 @server.route('/', methods=['POST'])
 def webhook():
     message = flask.request.get_json()
-    print('cool cool cool...')
+    reply('cool cool cool...')
+
+def reply(msg):
+	url = 'https://api.groupme.com/v3/bots/post'
+	template = {
+		'bot_id'		: bot_id,
+		'text'			: msg
+	}
+    headers = {'content-type': 'application/json'}
+    response = requests.post("https://api.groupme.com/v3/bots/post", data=json.dumps(template), headers=headers)
