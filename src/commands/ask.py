85a0e532d773f71c3d4caf116029e3424f6ff7fb
gpanda70 <gpanda@uga.edu>
2018-09-25 20:31:44 -0400

    Fixed Ask

--- src/commands/ask.py
+++ src/commands/ask.py
@@ -19,14 +19,20 @@ def search(msg):
     else:
         png_result = []
         result = []
-        pod0 = response['pod'][0]  # This is the question asked
-        pod1 = response['pod'][1]  # This pod might contain the answer
-        pod2 = response['pod'][2]  # This pod might contain the answer
-        print(json.dumps(pod0, indent=4))
-        print(json.dumps(pod1, indent=4))
-        print(json.dumps(pod2, indent=4))
+        pods = []
+        try:
+            pod0 = response['pod'][0]  # This is the question asked
+            pods.append(pod0)
+            pod1 = response['pod'][1]  # This pod might contain the answer
+            pods.append(pod1)
+            pod2 = response['pod'][2]  # This pod might contain the answer
+            pods.append(pod2)
+            print(json.dumps(pod0, indent=4))
+            print(json.dumps(pod1, indent=4))
+            print(json.dumps(pod2, indent=4))
+        except IndexError:
+            pass
 
-        pods = [pod0, pod1, pod2]
         try:
             result = [resolve_ls_or_dict(pod) for pod in pods ]
             print(result)
