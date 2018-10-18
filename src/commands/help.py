ad6c80b249a684f8e46e0842345344aae3917c19
gpanda70 <gpanda@uga.edu>
2018-10-18 02:35:17 -0400

    added analytics help to help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -5,13 +5,15 @@ def main(msg):
        '-gif {arg}',
        '-gif -random {arg}',
        '-ask {arg}',
-       '-help {arg}']
+       '-help {arg}'
+       '-analytics {arg}']
     y = ['returns the summary Wikipedia article of your arg.',
          'returns the first google image search result of your arg.',
          'returns a gif from giphy',
          'returns random gif from giphy.',
          'returns an answer for your question using wolframalpha.',
-         'returns this help menu']
+         'returns this help menu',
+         'returns link to analytics dashboard']
     h_menu = ('Disclaimer: This will look ugly on mobile.\nGo to github link for pretty answers.\nSource Code on ʕ•ᴥ•ʔ https://github.com/gpanda70/Jenkins ʕ•ᴥ•ʔ\n' + '-'*50)
     for i,j in zip(x,y):
         h_menu += ('\n{} {:>12}'.format(i,j))
