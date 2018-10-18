759756388579909c6129149ae219c839260e29a1
gpanda70 <gpanda@uga.edu>
2018-10-18 02:38:02 -0400

    added analytics help to help.py

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,12 +1,11 @@
 def main(msg):
-    #return('James was too lazy to print all the info here. So take a look at the github page\nhttps://github.com/gpanda70/Jenkins')
-    x=['-wiki {arg}',
-       '-img {arg}',
-       '-gif {arg}',
-       '-gif -random {arg}',
-       '-ask {arg}',
-       '-help {arg}'
-       '-analytics {arg}']
+    x = ['-wiki {arg}',
+         '-img {arg}',
+         '-gif {arg}',
+         '-gif -random {arg}',
+         '-ask {arg}',
+         '-help {arg}'
+         '-analytics {arg}']
     y = ['returns the summary Wikipedia article of your arg.',
          'returns the first google image search result of your arg.',
          'returns a gif from giphy',
@@ -15,7 +14,10 @@ def main(msg):
          'returns this help menu',
          'returns link to analytics dashboard']
     h_menu = ('Disclaimer: This will look ugly on mobile.\nGo to github link for pretty answers.\nSource Code on ʕ•ᴥ•ʔ https://github.com/gpanda70/Jenkins ʕ•ᴥ•ʔ\n' + '-'*50)
-    for i,j in zip(x,y):
-        h_menu += ('\n{} {:>12}'.format(i,j))
+
+    for i, j in zip(x, y):
+        h_menu += ('\n{} {:>12}'.format(i, j))
     return(h_menu)
+
+    
 print(main(''))
