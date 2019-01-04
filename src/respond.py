1fe0c05078a85e1f8700e508d6b28272eccd42e6
gpanda70 <gpanda@uga.edu>
2019-01-03 19:52:50 -0500

    refactored reply

--- src/respond.py
+++ src/respond.py
@@ -1,39 +1,34 @@
 import os
 import requests
 import json
-import math
 import importlib
 from importlib import import_module
 
 
 def reply(bot_id, msg):
     """This function responds to a user request"""
-
     url = 'https://api.groupme.com/v3/bots/post'
 
-    # If the groupme message is a command it first split the text.
-    # Next, it runs the command and stores the response to the command.
-    # If the response returns a list(multiple images) or a single response.
-    # It then sends a POST request to your group.
+    # This block is for regular responses
     if is_command(msg):
         command = msg.split()[0][1:]
         arg = msg.split()[1:]
         arg = ' '.join(arg)
         response = run_module(command, arg)
-
-        # Checks to see if response is a list of urls
-        if isinstance(response, list):
-            for num, r in enumerate(response):
-                print('WORKKKKKKKK')
-                png = image_service_process(r, num+1)
-                gif = {'type': 'image', 'url': png}
-                send_groupme_message(bot_id, url, gif=gif)
-        else:
-            send_groupme_message(bot_id, url, response)
     elif msg[0] == '-':
         send_groupme_message(bot_id, url, msg='This command does not exist.\nCheck -help')
     else:
         return
+
+    # Custom block for WolframAlpha responses that hecks to see if response
+    # is a list of urls
+    if isinstance(response, list):
+        for num, r in enumerate(response):
+            png = image_service_process(r, num+1)
+            png = {'type': 'image', 'url': png}
+            send_groupme_message(bot_id, url, png=png)
+    else:
+        send_groupme_message(bot_id, url, response)
 
 
 def is_command(msg):
@@ -59,12 +54,12 @@ def run_module(command, arg):
     return response
 
 
-def send_post(bot_id, url, msg='', gif=None):
+def send_post(bot_id, url, msg='', png=None):
     """Sends post request,containing command response, to the Groupme-Chat"""
 
     attachment = []
-    if gif:
-        attachment.append(gif)
+    if png:
+        attachment.append(png)
 
     template = {
         'bot_id': bot_id,
@@ -75,41 +70,43 @@ def send_post(bot_id, url, msg='', gif=None):
     requests.post(url, data=json.dumps(template), headers=headers)
 
 
-def send_groupme_message(bot_id, url, msg='', gif=None):
+def send_groupme_message(bot_id, url, msg='', png=None):
     SPLIT_SIZE = 1000
-    if len(msg) > 1000:
+    if len(msg) > SPLIT_SIZE:
+        # This is the best way to get api data with limited number of returns
         split_message_list = [msg[i:i+SPLIT_SIZE] for i in range(0, len(msg), SPLIT_SIZE)]
         for split_message in split_message_list:
             send_post(bot_id, url, split_message)
     else:
-        print('Reg Message')
-        send_post(bot_id, url, msg=msg, gif=gif)
+        send_post(bot_id, url, msg=msg, png=png)
 
 
-def save_images(gif_link, num):
-    # Saves the Wolfram image to ask_images directory
+def save_images(png_link, num):
+    """Saves the Wolfram image to ask_images directory"""
     head_path = os.path.dirname(__file__)
     wolf_img_path = 'ask_images/image{}.png'.format(num)
     with open(os.path.join(head_path, wolf_img_path), 'wb') as handle:
-        r = requests.get(gif_link, stream=True)
+        r = requests.get(png_link, stream=True)
         for block in r.iter_content(1024):
             handle.write(block)
 
 
 def process_image(access_token, num):
-    # Opens the Wolfram image and then sends it to the Groupme Image Service.
+    """
+    Opens the Wolfram image and then sends it to the Groupme Image Service.
+    """
     head_path = os.path.dirname(__file__)
     wolf_img_path = 'ask_images/image{}.png'.format(num)
     with open(os.path.join(head_path, wolf_img_path), 'rb') as handle:
         data = handle
         headers = {'X-Access-Token': access_token, 'Content-Type': 'image/png'}
-        gif_response = requests.post('https://image.groupme.com/pictures',
+        png_response = requests.post('https://image.groupme.com/pictures',
                                      headers=headers,
                                      data=data)
-        return gif_response
+        return png_response
 
 
-def image_service_process(gif_link, num):
+def image_service_process(png_link, num):
     """
         This function saves the wolframalpha image and processes them
         through Groupme's image service.
@@ -117,10 +114,9 @@ def image_service_process(gif_link, num):
     #access_token = os.getenv('access_token')  CHANGE BACK
     access_token = 'vac7dwISDpabX9tgHVlvpHXPAuRMTHHK59AvOgOM'
 
-    save_images(gif_link, num)
-    gif_response = process_image(access_token, num)
+    save_images(png_link, num)
+    png_response = process_image(access_token, num)
 
-    png_link = json.loads(gif_response.content)['payload']['picture_url']
-    print(png_link)
+    png_link = json.loads(png_response.content)['payload']['picture_url']
     # payload = {'type': 'image', 'url': png}
     return(png_link)
